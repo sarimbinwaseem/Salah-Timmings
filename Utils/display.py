7abe67fc6d1c27cdf4e43698b69c3118f6561dfe
sarimbinwaseem <sarimbinwaseem@github.com>
2024-02-05 13:37:49 +0500

    new class: Display and newmain

--- Utils/display.py
+++ Utils/display.py
@@ -6,62 +6,81 @@ from PIL import ImageFont
 
 
 class Display:
-	"""docstring for Display"""
+    """docstring for Display"""
 
-	def __init__(self):
-		super(Display, self).__init__()
-		self.RST = None
+    def __init__(self):
+        super(Display, self).__init__()
+        self.RST = None
 
-		# Note the following are only used with SPI:
-		self.DC = 23
-		self.SPI_PORT = 0
-		self.SPI_DEVICE = 0
-		self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+        # Note the following are only used with SPI:
+        self.DC = 23
+        self.SPI_PORT = 0
+        self.SPI_DEVICE = 0
+        self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+        self.X = 0
 
-		try:
-			self.font = ImageFont.truetype("Fonts/TheImpostor.ttf", 20)
-		except:
-			self.font = ImageFont.load_default()
+        try:
+            self.font = ImageFont.truetype("Fonts/TheImpostor.ttf", 20)
+        except:
+            self.font = ImageFont.load_default()
 
+    def get_disp_dimensions(self) -> tuple:
+        return (self.disp.width, self.disp.height)
 
-	def get_disp_dimensions(self) -> tuple:
+    def begin_display(self):
+        try:
+            self.disp.begin()
+        except OSError:
+            # Display may not be connected.
+            return -1
 
-		return (self.disp.width, self.disp.height)
+        else:
+            # Clear display.
+            self.disp.clear()
+            self.disp.display()
 
+    def set_image_support(self):
+        width, height = self.get_disp_dimensions()
 
-	def begin_display(self):
-		try:
-			self.disp.begin()
-		except OSError:
-			# Display may not be connected.
-			return -1
+        # First define some constants to allow easy resizing of shapes.
+        self.PADDING = 3
+        self.TOP = self.PADDING
+        self.BOTTOM = height - self.PADDING
+        # Move left to right keeping track of the current x position for drawing shapes.
+        self.X = 0
 
-		else:
-			# Clear display.
-			disp.clear()
-			disp.display()
+    def create_blank_image(self) -> Image.Image:
+        width, height = self.get_disp_dimensions()
+        # Create blank image for drawing.
+        # Make sure to create image with mode '1' for 1-bit color.
 
-	def create_blank_image(self) -> PIL.Image.Image:
+        image = Image.new("1", (width, height))
 
-		width, height = self.get_disp_dimensions()
-		# Create blank image for drawing.
-		# Make sure to create image with mode '1' for 1-bit color.
+        return image
 
-		image = Image.new("1", (width, height))
+    def get_draw(self, image: Image.Image) -> ImageDraw.ImageDraw:
+        # rawing object to draw on image.
+        draw = ImageDraw.Draw(image)
 
-		return image
+        width, height = self.get_disp_dimensions()
 
-	def get_draw(self) -> PIL.ImageDraw.ImageDraw:
-		#rawing object to draw on image.
-		draw = ImageDraw.Draw(image)
+        return draw
 
-		width, height = self.get_disp_dimensions()
+    def draw_rectangle(self, draw: ImageDraw.ImageDraw):
+        width, height = self.get_disp_dimensions()
 
-		return draw
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-	def draw_rectangle(self, draw: PIL.ImageDraw.ImageDraw):
-		# Draw a black filled box to clear the image.
-		draw.rectangle((0, 0, width, height), outline = 0, fill = 0)
+    def create_image(self, draw: ImageDraw.ImageDraw, current_time, next_salah_time):
+        draw.text((self.X, self.TOP), current_time, font=font, fill=255)
 
-	def draw_image(self, draw: PIL.ImageDraw.ImageDraw):
-		
+        draw.text((self.X, self.TOP + 28), next_salah_time, font=font, fill=255)
+
+    def display_image(self, image: Image.Image):
+        self.disp.image(image)
+        self.disp.display()
+
+    def clear(self):
+    	self.disp.clear()
+    	self.disp.display()
--- newmain.py
+++ newmain.py
@@ -3,14 +3,41 @@ import threading
 import time as timelib
 
 from Utils.salahtime import SalahTime
-
-print("Salah Timmings imported...")
-stime = SalahTime()
-
-thread = threading.Thread(target=stime.check_changes)
-thread.start()
+from Utils.display import Display
 
 
-while True:
+def main():
+	stime = SalahTime()
+	display = Display()
 
-	current_time, next_namaz_time = stime.get_all_times()
+	display.begin_display()
+	display.set_image_support()
+	image = display.create_blank_image()
+	draw = display.get_draw(image)
+	display.draw_rectangle(draw)
+	display.display_image(image)
+
+	thread = threading.Thread(target=stime.check_changes)
+	thread.start()
+
+	### Getting data and displaying times.
+	while True:
+		try:
+
+			current_time, next_salah_time = stime.get_all_times()
+			display.create_image(draw, current_time, next_salah_time)
+
+			# Display image.
+			display.display_image()
+			timelib.sleep(.4)
+
+		except KeyboardInterrupt:
+			print("Keyboard Interrupt... Exiting")
+			display.clear()
+			stime.check_changes_flag = False
+			thread.join()
+			sys.exit()
+
+
+if __name__ == "__main__":
+	main()
