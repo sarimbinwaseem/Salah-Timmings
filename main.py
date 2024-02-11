64fdd9405e028cadc54548aabc3089851a4a2603
Sarim Bin Waseem <42667306+sarimbinwaseem@users.noreply.github.com>
2024-02-11 12:56:59 +0500

    draw error tapped.
    draw variable was not defined in main function.

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ def display_loop(*args):
 	### Getting data and displaying times.
 	stime = args[1]
 	display = args[2]
+	draw = args[3]
 	for _ in range(13):
 		current_time, next_salah_time = stime.get_all_times()
-		# display.create_image(draw, current_time, next_salah_time)
+		display.create_image(draw, current_time, next_salah_time)
 
 		# Display image.
-		# display.display_image()
+		display.display_image()
 		timelib.sleep(0.4)
 
 		print(current_time, next_salah_time)
 
-#	display.clear()
+	display.clear()
 
 
 def main():
@@ -32,10 +33,6 @@ def main():
 	try:
 		stime = SalahTime()
 		display = Display()
-		# hard will be used later
-		hard = Hardware(display_loop, stime, display)
-		print("[+] Objects initialized...")
-
 		res = display.begin_display()
 		if res == -1:
 			print("[-] Display module may not be connected.")
@@ -43,6 +40,11 @@ def main():
 		image = display.create_blank_image()
 		draw = display.get_draw(image)
 		display.draw_rectangle(draw)
+
+		# hard will be used later
+		hard = Hardware(display_loop, stime, display, draw)
+		print("[+] Objects initialized...")
+
 		try:
 			display.display_image(image)
 		except OSError:
