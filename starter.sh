8688516d1f8c097d2489cba50acf1ec20b76ad45
sarimbinwaseem <sarimbinwaseem@github.com>
2024-02-11 17:55:11 +0500

    systemd service made for autostartup.

--- starter.sh
+++ starter.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Sleeping for 1 minute"
-sleep 1m
+# sleep 1m
 echo "Completed"
 
 echo "Now Running..."
-python3.11 /home/pi/Desktop/NamazTimingsDisplay/main.py
+cd /home/pi/Salah-Timmings
+/opt/python/3.11.4/bin/python3.11 main.py
 echo "Started Salah Timmings"
